50d65b776c6f9752675db7c640dbf3455fbc6c3a
陶志鹏 <taozhipeng@newerabc.com>
2020-11-21 00:46:16 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,12 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js"/>
+    <h1>TEST</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
